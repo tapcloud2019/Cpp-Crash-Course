cc78fd20693e11f117d9df2ae3dac0efe22e28ec
tapcloud2019 <tapcloud2019@gmail.com>
2022-12-19 15:08:19 +0800

    Listing 4-15

---
+++ CppCrashCourse/4-ObjLifeCycle/SimpleString.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdexcept>
+#include <cstdio>
+#include <cstring>
+
+struct SimpleString
+{
+	SimpleString(size_t max_size) : max_size{ max_size },
+		length{}
+	{
+		if (max_size == 0)
+			throw std::runtime_error("Max size must be at least 1");
+
+		buffer = new char[max_size];
+		buffer[0] = 0;
+	}
+
+	~SimpleString()
+	{
+		delete[] buffer;
+	}
+
+	void print(const char* tag) const
+	{
+		printf("%s: %s", tag, buffer);
+	}
+
+	bool append_line(const char* x)
+	{
+		const auto x_len = strlen(x);
+
+		//exceed the max string length
+		if (x_len + length + 2 > max_size)
+			return false;
+
+		std::strncpy(buffer + length, x, max_size - length);
+		length += x_len;
+		buffer[length++] = '\n';
+		buffer[length] = 0;
+
+		return true;
+	}
+
+private:
+	size_t max_size;
+	char* buffer;
+	size_t length;
+};
