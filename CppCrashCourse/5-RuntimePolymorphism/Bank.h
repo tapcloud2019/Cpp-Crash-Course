1e5caf299bc64945c998dd35574d362062992cde
tapcloud2019 <tapcloud2019@gmail.com>
2023-05-05 22:55:22 +0800

    Listing 5-14

--- CppCrashCourse/5-RuntimePolymorphism/Bank.h
+++ CppCrashCourse/5-RuntimePolymorphism/Bank.h
@@ -3,12 +3,19 @@
 
 struct Bank
 {
-	Bank(Logger& logger) : m_logger{ logger } {}
+	Bank(Logger* logger) : m_logger{ logger } {}
+
+	void set_logger(Logger* new_logger)
+	{
+		m_logger = new_logger;
+	}
+
 	void make_transfer(long from, long to, double amount)
 	{
-		m_logger.log_transform(from, to, amount);
+		if(m_logger)
+			m_logger->log_transform(from, to, amount);
 	}
 
 private:
-	Logger& m_logger;
+	Logger* m_logger;
 };
--- CppCrashCourse/5-RuntimePolymorphism/main.cpp
+++ CppCrashCourse/5-RuntimePolymorphism/main.cpp
@@ -1,16 +1,18 @@
 #include "Bank.h"
 
-void Listing5_12();
+void Listing5_14();
 
 int main()
 {
-	Listing5_12();
+	Listing5_14();
 }
 
-void Listing5_12()
+void Listing5_14()
 {
-	ConsoleLogger logger;
-	Bank bank{ logger };
+	ConsoleLogger console_logger;
+	FileLogger file_logger;
+	Bank bank{ &console_logger };
 	bank.make_transfer(1000, 2000, 49.95);
+	bank.set_logger(&file_logger);
 	bank.make_transfer(2000, 4000, 20.00);
 }
