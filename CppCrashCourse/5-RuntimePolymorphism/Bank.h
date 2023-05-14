a6d6becae0b243d98be1f8812c859a1f6586e35a
tapcloud2019 <tapcloud2019@gmail.com>
2023-05-14 22:41:24 +0800

    question 5_1-2-3

--- CppCrashCourse/5-RuntimePolymorphism/Bank.h
+++ CppCrashCourse/5-RuntimePolymorphism/Bank.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "Logger.h"
+#include "AccountDB.h"
+#include <iostream>
 
 struct Bank
 {
-	Bank(Logger* logger) : m_logger{ logger } {}
+	Bank(Logger* logger, AccountDB* accountDB) : m_logger{ logger },
+												m_accountDB{ accountDB }{}
 
 	void set_logger(Logger* new_logger)
 	{
@@ -16,6 +19,23 @@ struct Bank
 			m_logger->log_transform(from, to, amount);
 	}
 
+	void set_accountDB(AccountDB* new_accountDB)
+	{
+		m_accountDB = new_accountDB;
+	}
+
+	void set_amount(long id, double amount)
+	{
+		m_accountDB->set_amount(id, amount);
+	}
+
+	void retrieve_amount(long id)
+	{
+		double balance = m_accountDB->retrieve_amount(id);
+		std::cout << "account: " << id << " balance: " << balance << std::endl;
+	}
+
 private:
 	Logger* m_logger;
+	AccountDB* m_accountDB;
 };
--- CppCrashCourse/5-RuntimePolymorphism/main.cpp
+++ CppCrashCourse/5-RuntimePolymorphism/main.cpp
@@ -1,18 +1,37 @@
 #include "Bank.h"
 
 void Listing5_14();
+void Question5_1_2_3();
 
 int main()
 {
-	Listing5_14();
+	Question5_1_2_3();
+}
+
+void Question5_1_2_3()
+{
+	ConsoleLogger console_logger;
+	InMemoryAccountDB in_mem_accountDB;
+	Bank bank{ &console_logger, &in_mem_accountDB };
+	bank.retrieve_amount(1234);
+	bank.retrieve_amount(2345);
+	bank.retrieve_amount(3456);
+	bank.set_amount(1234, 1000000);
+	bank.set_amount(2345, 1500000);
+	bank.set_amount(3456, 1800000);
+	bank.retrieve_amount(1234);
+	bank.retrieve_amount(2345);
+	bank.retrieve_amount(3456);
 }
 
 void Listing5_14()
 {
 	ConsoleLogger console_logger;
 	FileLogger file_logger;
-	Bank bank{ &console_logger };
+	/*
+	Bank bank{&console_logger};
 	bank.make_transfer(1000, 2000, 49.95);
 	bank.set_logger(&file_logger);
 	bank.make_transfer(2000, 4000, 20.00);
+	*/
 }
