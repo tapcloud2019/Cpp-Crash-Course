46c3077bbe79424a9ac341d055abf60a8cb12969
tapcloud2019 <tapcloud2019@gmail.com>
2023-12-23 14:17:32 +0800

    Listing 7-6

--- CppCrashCourse/7-Expressions/main.cpp
+++ CppCrashCourse/7-Expressions/main.cpp
@@ -1,14 +1,54 @@
 #include <cstdio>
 #include <limits>
 #include "CheckedInteger.h"
+#include "Heap.h"
 
 void Listing7_1();
 int confusing(int&);
 void Listing7_3();
+Heap heap;
+void* operator new(size_t n_bytes);
+void operator delete(void* p);
+void Listing7_6();
 
 int main()
 {
-	Listing7_3();
+	Listing7_6();
+}
+
+
+void Listing7_6()
+{
+	printf("Buckets: %p\n", heap.buckets);
+	auto breakfast = new unsigned int{ 0xC0FFEE };
+	auto dinner = new unsigned int{ 0xDEADBEEF };
+	printf("breakfast: %p 0x%x\n", breakfast, *breakfast);
+	printf("Dinner: %p 0x%x\n", dinner, *dinner);
+	delete breakfast;
+	delete dinner;
+
+	try 
+	{
+		while (true)
+		{
+			new char;
+			printf("Allocated a char \n");
+		}
+	}
+	catch (const std::bad_alloc)
+	{
+		printf("std::bad_alloc caught \n");
+	}
+}
+
+void* operator new(size_t n_bytes)
+{
+	return heap.allocate(n_bytes);
+}
+
+void operator delete(void* p)
+{
+	return heap.free(p);
 }
 
 void Listing7_3()
